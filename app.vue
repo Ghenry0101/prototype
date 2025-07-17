222ae5fac21399c587e335f00e90b8c10ca77b0a
Ghatfan_kun <104193750+Ghenry0101@users.noreply.github.com>
2025-07-17 11:52:18 +0700

    njg

--- app.vue
+++ app.vue
@@ -1,21 +1,48 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { fetchAllLocations } from './composables/useApi'
+
+const locations = ref([])
+const users = ref([])
+const mapPageRef = ref(null)
+
+onMounted(async () => {
+  const responseData = await fetchAllLocations()
+  console.log(responseData)
+  users.value = responseData.locations
+  locations.value = responseData.locations
+
+  // Ambil hanya username yang tidak kosong
+  locations.value = responseData.locations.filter(item => item.username)
+  users.value = locations.value.map(item => ({
+    username: item.username,
+    latitude: item.latitude,
+    longitude: item.longitude,
+  }))
+})
+
+async function handleClickUser(user) {
+  if (!user.latitude || !user.longitude) return
+  mapPageRef.value.setLocation(user)
+}
+</script>
+
 <template>
   <div class="flex h-screen">
     <!-- Sidebar User List -->
     <div class="w-64 flex flex-col border-r">
       <h2 class="font-bold text-lg p-4 border-b">List User</h2>
       <div class="overflow-auto flex-grow">
-<ul>
-  <li 
-    v-for="user in users" 
-    :key="user.id" 
-    @click="handleClickUser(user)"
-    class="p-2 cursor-pointer hover:bg-gray-100"
-  >
-    {{ user.username || user.name }}
-  </li>
-</ul>
-
-
+        <ul>
+          <li 
+            v-for="user in users" 
+            :key="user.username" 
+            @click="handleClickUser(user)"
+            class="p-2 cursor-pointer hover:bg-gray-100"
+          >
+            {{ user.username }}
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -25,32 +52,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-import { onMounted, ref } from 'vue'
-import { fetchUsers, fetchUserLocationByName } from './composables/useApi'
-
-const locations = ref([])
-const users = ref([])
-const mapPageRef = ref(null)
-
-onMounted(async () => {
-  const responseData = await fetchUsers()
-  console.log(responseData)
-  users.value = responseData.users
-  // console.log('Users:', users.value) // Log pengguna setelah pengisian
-  locations.value = await fetchAllLocations()
-})
-
-async function handleClickUser(user) {
-  const data = await fetchUserLocationByName(user.username)
-  console.log(data)
-  if (data && data.length > 0) {
-    mapPageRef.value.setLocation(data[0])
-  }
-}
-
-// const handleClickUser = (location) => {
-//   console.log(location)
-// }
-</script>
--- components/MapPage.vue
+++ components/MapPage.vue
@@ -1,33 +1,82 @@
 <template>
-  <div id="map" style="height: 500px;"></div>
+  <div id="map" style="height: 100vh;"></div>
 </template>
 
 <script setup>
 import { ref, onMounted, defineExpose } from 'vue'
 import 'leaflet/dist/leaflet.css'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref as dbRef, onValue } from "firebase/database";
 
-let map = null
-const marker = ref(null)
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDz0JFiuq5sYcUUIHRxR_Fb7nNZRGDqdk8",
+  authDomain: "locator-dccf6.firebaseapp.com",
+  databaseURL: "https://locator-dccf6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "locator-dccf6",
+  storageBucket: "locator-dccf6.appspot.com",
+  messagingSenderId: "637860668160",
+  appId: "1:637860668160:web:b5ec092372e2812f99c9e5",
+  measurementId: "G-8CN84BCJCD"
+};
+
+// Firebase Init
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+let map = ref(null)
+const markers = ref({})
+let L
 
 onMounted(async () => {
   if (process.client) {
-    const L = await import('leaflet')
-    map = L.map('map').setView([-6.2, 106.816666], 5)
+    L = await import('leaflet')
+    map.value = L.map('map').setView([-6.2, 106.816666], 5)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
-    }).addTo(map)
+    }).addTo(map.value)
 
-    marker.value = L.marker([-6.2, 106.816666]).addTo(map)
-      .bindPopup('Jakarta')
+    listenRealtimeLocations()
   }
 })
 
-// Method ini buat dipanggil dari luar komponen
+function listenRealtimeLocations() {
+  const locationsRef = dbRef(db, 'locations')
 
-function setLocation(location) {
-  marker.value.setLatLng([location.latitude, location.longitude]).bindPopup(location.username).openPopup()
+  onValue(locationsRef, (snapshot) => {
+    const data = snapshot.val()
+    if (!data) return
 
-  map.value.setView([location.latitude, location.longitude], 13)
+    Object.values(data).forEach(user => {
+      updateMarker(user)
+    })
+  })
 }
-defineExpose({ setLocation })
+
+function updateMarker(user) {
+  if (!map.value) return;
+  const key = user.username
+  if (markers.value[key]) {
+    markers.value[key].setLatLng([user.latitude, user.longitude])
+  } else {
+    markers.value[key] = L.marker([user.latitude, user.longitude])
+      .addTo(map.value)
+      .bindPopup(user.username)
+  }
+}
+
+function setLocation(user) {
+  if (!map.value) return;
+  updateMarker(user)
+  map.value.setView([user.latitude, user.longitude], 16)
+}
+
+defineExpose({ setLocation, updateMarker })
 </script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
